c62aead2f537952959a7a7628e3cdc378d459518
McYvar <yvarftoorop@hotmail.com>
2022-11-17 15:11:08 +0100

    Bank was added

---
+++ Bankrekening/Bank/Bankaccount.cpp
@@ -0,0 +1,60 @@
+﻿#include "Bankaccount.h"
+
+#include <ostream>
+
+Bankaccount::Bankaccount(double startingBalance = 0)
+{
+    balance = startingBalance;
+    history.clear();
+}
+
+Bankaccount::~Bankaccount()
+= default;
+
+Bankaccount::Bankaccount(const Bankaccount& other)
+{
+    balance = other.balance;
+}
+
+Bankaccount& Bankaccount::operator=(const Bankaccount& otherAccount)
+= default;
+
+double Bankaccount::GetBalance() const
+{
+    return balance;
+}
+
+Bankaccount Bankaccount::operator+(const Transactions& transaction)
+{
+    history.push_back(transaction);
+    return { balance + transaction.GetAmount() };
+}
+
+Bankaccount& Bankaccount::operator+=(const Transactions& transaction)
+{
+    history.push_back(transaction);
+    balance += transaction.GetAmount();
+    return *this;
+}
+
+string Bankaccount::ShowTransactions() const
+{
+    string result = "\nTransaction history:\n";
+    if (history.empty()) result += "No transactions in account\n";
+    else
+    {
+        for (auto i = history.begin(); i != history.end(); ++i)
+        {
+            result += i->ToString() + "\n";
+        }
+    }
+    result += "Current balance: " + to_string(balance);
+    return result;
+}
+
+std::ostream& operator<<(std::ostream& os, const Bankaccount& bankaccount)
+{
+    os << bankaccount.ShowTransactions();
+
+    return os;
+}
---
+++ Bankrekening/Bank/Transactions.cpp
@@ -0,0 +1,42 @@
+﻿#include "Transactions.h"
+
+Transactions::Transactions(double amount, uint8_t day, uint8_t month, uint16_t year)
+{
+    this->amount = amount;
+    this->day = day;
+    this->month = month;
+    this->year = year;
+}
+
+double Transactions::GetAmount() const
+{
+    return amount;
+}
+
+uint8_t Transactions::GetDay() const
+{
+    return day;
+}
+
+uint8_t Transactions::GetMonth() const
+{
+    return month;
+}
+
+uint16_t Transactions::GetYear() const
+{
+    return year;
+}
+
+string Transactions::ToString() const
+{
+    string result = "Transaction on (mm/dd/yyy): ";
+    if (month > 9) result += to_string(month) + "/";
+    else result += "0" + to_string(month) + "/";
+
+    if (day > 9) result += to_string(day) + "/";
+    else result += "0" + to_string(day) + "/";
+
+    result += to_string(year) + "\nAmount: " + to_string(amount) + "\n";
+    return result;
+}
